75b107fe35adddb367b19855591a430b0a8aa5e1
Parav Mahajan <Parav.deliverable@gmail.com>
2026-01-14 18:15:43 +0530

    onboard form fix

--- lib/app/modules/on_boarding/views/on_boarding_view.dart
+++ lib/app/modules/on_boarding/views/on_boarding_view.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:samsung_community_mobile/app/routes/app_pages.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 
 import '../../../common/services/analytics_service.dart';
 import '../../../data/constants/app_button.dart';
@@ -41,91 +40,85 @@ class OnBoardingView extends GetView<OnBoardingController> {
               bottom: true,
               child: Align(
                 alignment: const AlignmentDirectional(0, 0),
-                child: SingleChildScrollView(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.max,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      SizedBox(
-                        height: MediaQuery.of(context).size.height * .66,
-                      ),
-                      SizedBox(
-                        width: 313.w,
-                        height: 32.h,
-                        child: Center(
-                          child: Text(
-                            'welcomeDescription'.tr,
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              fontSize: 12.sp,
-                              fontWeight: FontWeight.w400,
-                              letterSpacing: 0,
-                              color: AppColors.white,
-                              height: 22 / 14,
-                            ),
-                            textScaler: const TextScaler.linear(1.0),
-                            maxLines: 2,
-                            overflow: TextOverflow.visible,
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(height: MediaQuery.of(context).size.height * .66),
+                    SizedBox(
+                      width: 313.w,
+                      child: Center(
+                        child: Text(
+                          'welcomeDescription'.tr,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 12.sp,
+                            fontWeight: FontWeight.w400,
+                            letterSpacing: 0,
+                            color: AppColors.white,
+                            height: 22 / 14,
                           ),
+                          textScaler: const TextScaler.linear(1.0),
+                          maxLines: 2,
+                          overflow: TextOverflow.visible,
                         ),
                       ),
-                      SizedBox(height: 90.h),
-                      AppButton(
-                        onTap: () => controller.clickOnSignUpWithGoogleButton(),
-                        text: 'Sign up with google'.tr,
-                        iconPath: AppImages.googleIcon,
-                        iconSize: 20.w,
-                      ),
-                      SizedBox(height: 30.h),
-                      AppButton(
-                        onTap: () {
-                          AnalyticsService.logButtonClick(
-                            screenName: 'Main screen',
-                            buttonName: 'Login',
-                            eventName: 'main_screen_click',
-                          );
-                          controller.clickOnLogInButton();
-                        },
-                        text: 'logIn'.tr,
-                      ),
-                      SizedBox(height: 30.h),
-                      RichText(
-                        textScaler: const TextScaler.linear(1.0),
-                        text: TextSpan(
-                          children: [
-                            TextSpan(
-                              text: 'dontHaveAccount'.tr,
-                              style: TextStyle(
-                                color: AppColors.linkBlue,
-                                letterSpacing: 0,
-                                fontSize: 16.sp,
-                              ),
+                    ),
+                    SizedBox(height: 25.h),
+                    AppButton(
+                      onTap: () => controller.clickOnSignUpWithGoogleButton(),
+                      text: 'Sign up with google'.tr,
+                      iconPath: AppImages.googleIcon,
+                      iconSize: 20.w,
+                    ),
+                    SizedBox(height: 20.h),
+                    AppButton(
+                      onTap: () {
+                        AnalyticsService.logButtonClick(
+                          screenName: 'Main screen',
+                          buttonName: 'Login',
+                          eventName: 'main_screen_click',
+                        );
+                        controller.clickOnLogInButton();
+                      },
+                      text: 'logIn'.tr,
+                    ),
+                    SizedBox(height: 20.h),
+                    RichText(
+                      textScaler: const TextScaler.linear(1.0),
+                      text: TextSpan(
+                        children: [
+                          TextSpan(
+                            text: 'dontHaveAccount'.tr,
+                            style: TextStyle(
+                              color: AppColors.linkBlue,
+                              letterSpacing: 0,
+                              fontSize: 16.sp,
                             ),
-                            TextSpan(
-                              text: 'signUp'.tr,
-                              style: TextStyle(
-                                color: AppColors.linkBlue,
-                                fontSize: 16.sp,
-                                letterSpacing: 0,
-                                fontWeight: FontWeight.bold,
-                              ),
-                              recognizer: TapGestureRecognizer()
-                                ..onTap = () {
-                                  // Log button click event
-                                  AnalyticsService.logButtonClick(
-                                    screenName: 'Main screen',
-                                    buttonName: 'signup',
-                                    eventName: 'main_screen_click',
-                                  );
-                                  Get.toNamed(Routes.SIGN_UP);
-                                },
+                          ),
+                          TextSpan(
+                            text: 'signUp'.tr,
+                            style: TextStyle(
+                              color: AppColors.linkBlue,
+                              fontSize: 16.sp,
+                              letterSpacing: 0,
+                              fontWeight: FontWeight.bold,
                             ),
-                          ],
-                        ),
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                AnalyticsService.logButtonClick(
+                                  screenName: 'Main screen',
+                                  buttonName: 'signup',
+                                  eventName: 'main_screen_click',
+                                );
+                                Get.toNamed(Routes.SIGN_UP);
+                              },
+                          ),
+                        ],
                       ),
-                      SizedBox(height: 20.h),
-                    ],
-                  ),
+                    ),
+                    SizedBox(height: 20.h),
+                  ],
                 ),
               ),
             ),
